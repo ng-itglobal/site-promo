d3656b05bb8aa5abf220e5735bb869c2f7711d0e
Nikolai Goloveshkin <ng@itglobal.ru>
2019-12-27 18:04:06 +0300

    модальное окно, скрипт отправки данных формы

--- js/modal.js
+++ js/modal.js
@@ -10,10 +10,17 @@
     id.css("left", winW / 2 - id.width() / 2);
 
     id.fadeIn();
+
+    $("input").prop("disabled", true);
+    $("textarea").prop("disabled", true);
+    $(".page").fadeTo(500, 0.5);
   });
 
   $(".closeModal").click(function(e) {
     e.preventDefault();
     $(".modal").fadeOut();
+    $("input").prop("disabled", false);
+    $("textarea").prop("disabled", false);
+    $(".page").fadeTo(500, 1);
   });
 })(jQuery);
