91947d4a5821bbaae49d344684937bf72e9f9bf0
Goloveshkin <kolya-mp19@mail.ru>
2020-01-15 00:08:43 +0300

    fix бергер меню

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -1,28 +1,30 @@
 $(document).ready(function($) {
-  $(".containerIconBurger").click(function() {
+  $(".topnavHeadMenuIconOpen").click(function() {
+    $(".topnavHeadMenuIconOpen").css("display", "none");
+    $(".topnavHeadMenuIconClose").css("display", "block");
+    $(".headMobLinkS").css("display", "block");
+
     if (parseInt($(".headMob").css("height")) === $(window).height()) {
       $(".headMob").css("height", "auto");
+      $(".wrapLinksMobMenu").css("display", "none");
     } else {
       $(".headMob").css("height", "100vh");
+      $(".wrapLinksMobMenu").css("display", "");
     }
-  });
+
+  })
+
+  $(".topnavHeadMenuIconClose").click(function() {
+    $(".topnavHeadMenuIconClose").css("display", "none");
+    $(".topnavHeadMenuIconOpen").css("display", "block");
+    $(".headMobLinkS").css("display", "none");
+    $(".headMob").css("height", "auto");
+  })
 
   $(".headMobLink").click(function() {
-    $(".containerIconBurger").click();
-    $(".headMobLinks").css("display", "none");
+    $(".headMobLinkS").css("display", "none");
     $(".headMob").css("height", "auto");
+    $(".topnavHeadMenuIconClose").css("display", "none");
+    $(".topnavHeadMenuIconOpen").css("display", "block");
   });
 })(jQuery);
-
-function actionBurgMenu() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
-function actionIconBurgMenu(x) {
-  x.classList.toggle("change");
-}
